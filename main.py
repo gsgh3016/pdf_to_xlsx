aa547a9bccc22bac6aeea307477048947d390d4e
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-03-12 23:25:09 +0900

    Change dependencies
    - change PyMuPDF module to pdfplumber for converting pdf content to pd.DataFrame

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ EXCEL_FILE_DIR = "excel/"
 
 
 from utils import *
-import fitz
+import pdfplumber
 
 
 if __name__ == "__main__":
     check_dir(PDF_FILE_DIR)
     pdf_files = scan_files(PDF_FILE_DIR, "pdf")
-    text = ""
-    for pdf_file in pdf_files:
-        doc = fitz.open(pdf_file)
-        for page in doc:
-            text += page.get_text()
+    for pdf in pdf_files:
+        with pdfplumber.open(pdf) as p:
+            for page in p.pages:
+                table = page.extract_table()
+                df = pd.DataFrame(table[1:], columns=table[0])
+                print(df)
             
     excel_files = scan_files(EXCEL_FILE_DIR, "xlsx")
     for excel_file in excel_files:
